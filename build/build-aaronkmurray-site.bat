326e19bfa40789bc0e3c7e48dbabdcbe2ca0fb17
Aaron Murray <akmurray@gmail.com>
2012-09-17 15:01:32 -0500

    Post 12 changes for generating thumbnails

--- build/build-aaronkmurray-site.bat
+++ build/build-aaronkmurray-site.bat
@@ -1,11 +1,33 @@
-@echo off
-REM Do all of the prep-work steps required to build the aaronkmurray.com site
+@ECHO OFF
+REM Do all of the prep-work steps required to build the aaronkmurray.com site 
+
+
+REM Make post screenshot thumbnails if necessary
+FOR /F %%A IN ('dir /b "../../aaronkmurray-blog/img/blog/screenshots/" ^|findstr /liv "thumb"') DO (
+
+  REM so that we can update variables inside a loop. Those variables are wrapped in "!" instead of "%"
+  SETLOCAL ENABLEDELAYEDEXPANSION
+  
+  SET thumbName=%%~nA-thumb-100.png
+  SET thumbPath=../../aaronkmurray-blog/img/blog/screenshots/
+  SET thumbPathAndName=!thumbPath!!thumbName!
+  
+  IF NOT EXIST !thumbPathAndName! (
+    ECHO Creating thumbnail for !thumbName!
+    convert.exe -thumbnail 100 !thumbPath!%%A !thumbPathAndName!
+  ) ELSE (
+    REM ECHO Already created thumbnail for !thumbName!
+  )
+)
+
 
 REM use RSSGEN to build rss feed
 rssgen.exe -s=../../aaronkmurray-blog/index.html -o=../../aaronkmurray-blog/feeds/feed-rss.xml -f=rss
 
+
 REM use RSSGEN to build atom feed
 rssgen.exe -s=../../aaronkmurray-blog/index.html -o=../../aaronkmurray-blog/feeds/feed-atom.xml -f=atom
+
 
 REM use imgsqz to losslessly compress the filesize of images
 imgsqz.exe -s=../../aaronkmurray-blog/ -debug
