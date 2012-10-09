755fdba61a26cae49e52a3bcded556cefe2e5e92
Aaron Murray <akmurray@gmail.com>
2012-10-09 14:11:28 -0500

    Added feature to imgsqz to also compress jpgs

--- img/imgsqz/imgsqz/Program.cs
+++ img/imgsqz/imgsqz/Program.cs
@@ -19,6 +19,9 @@ namespace imgsqz
     /// 
     /// For a GUI compressor, I prefer PNGGauntlet ( http://pnggauntlet.com/ ) 
     ///     which uses pngout.exe by the awesome Ken Silverman: http://advsys.net/ken/utils.htm#pngout
+    /// 
+    /// For reducing filesize of JPG/GIF, currently using ImageMagick  ( http://www.imagemagick.org/ ) 
+    /// 
     /// </summary>
     class Program
     {
@@ -41,7 +44,7 @@ namespace imgsqz
             string pathSource = null;
             bool forceRecalc = false;
             bool recurseDirs = true;
-            string searchPattern = "*.png";
+            string searchPattern = "*.png|*.jpg|*.jpeg";
             string pathFileStatus = "filestatus.json";
             
 
@@ -51,8 +54,8 @@ namespace imgsqz
 
             var p = new OptionSet() {
                 { "s|pathSource=", "[optional, default=current folder]",  x => pathSource = x },
-                { "f|forceRecalc", "[optional, force a recalculation of all images, default=" + forceRecalc + "]",   x => forceRecalc = (x != null) || bool.Parse(x)},
-                { "r|recurseDirs", "[optional, recursively compress images in subfolders, default="+recurseDirs + "]",   x => recurseDirs = (x == null) || bool.Parse(x)},
+                { "f|forceRecalc=", "[optional, force a recalculation of all images, default=" + forceRecalc + "]",   x => forceRecalc = (x != null) || bool.Parse(x)},
+                { "r|recurseDirs=", "[optional, recursively compress images in subfolders, default="+recurseDirs + "]",   x => recurseDirs = (x == null) || bool.Parse(x)},
                 { "pat|searchPattern=", "[optional, filename pattern match expression, default="+searchPattern + "]",   x => searchPattern = x},
                 { "pfs|pathFileStatus=", "[optional, file to store file compression status info, default="+pathFileStatus + "]",   x => pathFileStatus = x},
                 
@@ -81,16 +84,18 @@ namespace imgsqz
             }
 
 
-
             Console.WriteLine();
-            Console.WriteLine("imgsqz by @AaronKMurray using options:");
-            Console.WriteLine("\tpathSource:\t" + pathSource);
-            Console.WriteLine("\trecurseDirs:\t" + recurseDirs);
-            Console.WriteLine("\tsearchPattern:\t" + searchPattern);
-            Console.WriteLine("\tforceRecalc:\t" + forceRecalc);
-            Console.WriteLine("\tpathFileStatus:\t" + pathFileStatus);
-            Console.WriteLine();
-
+            Console.WriteLine("imgsqz by @AaronKMurray");
+            if (showDebug)
+            {
+                Console.WriteLine("using options:");
+                Console.WriteLine("\tpathSource:\t" + pathSource);
+                Console.WriteLine("\trecurseDirs:\t" + recurseDirs);
+                Console.WriteLine("\tsearchPattern:\t" + searchPattern);
+                Console.WriteLine("\tforceRecalc:\t" + forceRecalc);
+                Console.WriteLine("\tpathFileStatus:\t" + pathFileStatus);
+                Console.WriteLine();
+            }
 
             // --- FIND THE FILES TO WORK ON --- //
 
@@ -101,7 +106,15 @@ namespace imgsqz
             string[] filepaths;
             try
             {
-                filepaths = Directory.GetFiles(pathSource, searchPattern, searchOption);
+                var patterns = searchPattern.Split(new [] {"|"}, StringSplitOptions.RemoveEmptyEntries);
+                var allpaths = new List<string>();
+                foreach (var pattern in patterns)
+                {
+                    var paths = Directory.GetFiles(pathSource, pattern, searchOption);
+                    if (paths.Length > 0)
+                        allpaths.AddRange(paths);
+                }
+                filepaths = allpaths.ToArray();
             }
             catch (Exception ex)
             {
@@ -114,7 +127,7 @@ namespace imgsqz
                 for (int i=0; i<filepaths.Length;i++)
                     Console.WriteLine("{0}: {1}", i + 1, filepaths[i]);
 
-            Console.WriteLine("Matched {0} files to compress", filepaths.Length);
+            Console.WriteLine("Matched {0} file(s) to compress", filepaths.Length);
 
 
             // --- ENSURE/LOAD UP THE STATUS FILE --- //
@@ -258,29 +271,51 @@ namespace imgsqz
                 sw.Start();
 
                 var fi = new FileInfo(path);
+                var ext = fi.Extension.ToLower();
+                var isPng = ext.EndsWith("png");
+                var isJpg = ext.EndsWith("jpg") || ext.EndsWith("jpeg");
+
                 result.StartSize = fi.Length;
 
-                //first try optipng
-                var resultsOptipng = RunOptipng(path);
-                result.AddNote(resultsOptipng);
-                fi = new FileInfo(path);
-                result.EndSize = fi.Length;
-                var step1ms = sw.ElapsedMilliseconds;
-                result.AddNote("Size after Optipng: " + fi.Length + " bytes (" + (result.StartSize - result.EndSize) + " savings), took " + sw.ElapsedMilliseconds + " milliseconds");
+                if (isPng)
+                {
 
-                //last ditch effort to crush size using pngout
-                var startSize = result.EndSize;
-                var resultsPngout = RunPngout(path);
-                result.AddNote(resultsPngout);
-                fi = new FileInfo(path);
-                result.EndSize = fi.Length;
-                result.AddNote("Size after Pngout: " + fi.Length + " bytes (" + (startSize - result.EndSize) + " savings), took " + (sw.ElapsedMilliseconds - step1ms) + " milliseconds");
+                    //first try optipng
+                    var resultsOptipng = RunOptipng(path);
+                    result.AddNote(resultsOptipng);
+                    fi = new FileInfo(path);
+                    result.EndSize = fi.Length;
+                    var step1ms = sw.ElapsedMilliseconds;
+                    result.AddNote("Size after Optipng: " + fi.Length + " bytes (" + (result.StartSize - result.EndSize) +
+                                   " savings), took " + step1ms + " milliseconds");
+
+                    //last ditch effort to crush size using pngout
+                    var startSize = result.EndSize;
+                    var resultsPngout = RunPngout(path);
+                    result.AddNote(resultsPngout);
+                    fi = new FileInfo(path);
+                    result.EndSize = fi.Length;
+                    result.AddNote("Size after Pngout: " + fi.Length + " bytes (" + (startSize - result.EndSize) +
+                                   " savings), took " + (sw.ElapsedMilliseconds - step1ms) + " milliseconds");
+
+                } 
+                else if (isJpg)
+                {
+                    var resultsJpg = CompressJpg(path);
+                    result.AddNote(resultsJpg);
+                    fi = new FileInfo(path);
+                    result.EndSize = fi.Length;
+                    var step1ms = sw.ElapsedMilliseconds;
+                    result.AddNote("Size after JPG Compression: " + fi.Length + " bytes (" + (result.StartSize - result.EndSize) +
+                                   " savings), took " + step1ms + " milliseconds");
+                    
+                }
 
                 result.SizeDelta = result.StartSize - result.EndSize;
                 if (result.SizeDelta > 0)
                     result.StatusCode = 1;
                 else
-                    result.StatusCode = 3; 
+                    result.StatusCode = 3;
             } 
             catch (Exception ex)
             {
@@ -315,19 +350,142 @@ namespace imgsqz
             return JsonConvert.DeserializeObject<List<ImageActionStatus>>(json);
         }
 
-        
-        static string RunOptipng(string pImagePath) 
+
+        static string RunOptipng(string pImagePathInput) 
         {
-            string pngoutExePath = "optipng.exe";
-            var output = ProcessHelper.RunProcessAndReturnOutput(pngoutExePath, "-o7 -v " + pImagePath);
+            string exePath = "optipng.exe";
+            var output = ProcessHelper.RunProcessAndReturnOutput(exePath, "-o7 -v " + pImagePathInput);
             return output;
         }
 
-        static string RunPngout(string pImagePath) 
+        static string RunPngout(string pImagePathInput) 
         {
-            string pngoutExePath = "pngout.exe";
-            var output = ProcessHelper.RunProcessAndReturnOutput(pngoutExePath, "/y " + pImagePath);
+            string exePath = "pngout.exe";
+            var output = ProcessHelper.RunProcessAndReturnOutput(exePath, "/y " + pImagePathInput);
             return output;
+        }
+
+        static string RunImageMagick(string pImagePathInput, string pArguments, string pImagePathOutput = null)
+        {
+            if (string.IsNullOrWhiteSpace(pImagePathOutput))
+                pImagePathOutput = pImagePathInput; //default to overwrite original
+
+            string exePath = "convert.exe";
+            var output = ProcessHelper.RunProcessAndReturnOutput(exePath, pArguments + " " + pImagePathInput + " " + pImagePathOutput);
+            return output;
+        }
+
+        /// <summary>
+        /// Compress a jpg by trying baseline and progressive options - keeping the smaller and overwriting the original faile
+        /// </summary>
+        /// <param name="pImagePathInput"></param>
+        /// <param name="pImagePathOutput">if null/empty, overwrite original file</param>
+        /// <param name="pIsDebug">if true, adds extra debug-level results text </param>
+        /// <returns></returns>
+        static string CompressJpg(string pImagePathInput, string pImagePathOutput = null, bool pIsDebug = false)
+        {
+            if (string.IsNullOrWhiteSpace(pImagePathOutput))
+                pImagePathOutput = pImagePathInput; //default to overwrite original
+
+
+            var sb = new StringBuilder();
+
+            FileInfo fiSource = new FileInfo(pImagePathInput), fiBaseline = null, fiProgressive = null;
+
+            var guid = Guid.NewGuid();
+            var filepathBaseline = fiSource.Name + "-" + guid + "-baseline.jpg";
+            var filepathProgressive = fiSource.Name + "-" + guid + "-progressive.jpg";
+
+            var outputB = RunImageMagick(pImagePathInput, "-strip", filepathBaseline);
+            if (pIsDebug)
+                sb.AppendLine(outputB);
+
+            var outputP = RunImageMagick(pImagePathInput, "-strip -interlace Plane", filepathProgressive);
+            if (pIsDebug)
+                sb.AppendLine(outputP);
+
+            bool usingBaseline = false, usingProgressive = false;
+
+            var smallestBytes = fiSource.Length;
+
+            if (File.Exists(filepathBaseline))
+            {
+                fiBaseline = new FileInfo(filepathBaseline);
+                if (fiBaseline.Length < smallestBytes)
+                {
+                    usingProgressive = false;
+                    usingBaseline = true;
+                    smallestBytes = fiBaseline.Length;
+                }
+            }
+
+            if (File.Exists(filepathProgressive))
+            {
+                fiProgressive = new FileInfo(filepathProgressive);
+                if (fiProgressive.Length < smallestBytes)
+                {
+                    usingBaseline = false;
+                    usingProgressive = true;
+                    smallestBytes = fiProgressive.Length;
+                }
+            }
+
+            if (usingBaseline)
+            {
+                sb.AppendLine("Compressed jpg using Baseline method. Saved " + (fiSource.Length - smallestBytes) + " bytes");
+                ReplaceImage(fiSource, fiBaseline);
+            }
+            else if (usingProgressive)
+            {
+                sb.AppendLine("Compressed jpg using Progressive method. Saved " + (fiSource.Length - smallestBytes) + " bytes");
+                ReplaceImage(fiSource, fiProgressive);
+            }
+            else
+            {
+                sb.AppendLine("Unable to compress jpg further. " + pImagePathInput);
+            }
+
+            File.Delete(filepathBaseline);
+            File.Delete(filepathProgressive);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Use pFiNew to replace pFiOriginal
+        /// </summary>
+        /// <param name="pFiOriginal"></param>
+        /// <param name="pFiNew"></param>
+        /// <returns></returns>
+        private static bool ReplaceImage(FileInfo pFiOriginal, FileInfo pFiNew)
+        {
+            var targetFullName = pFiOriginal.FullName;
+            var tempfile = pFiOriginal.Name + "-" + Guid.NewGuid() + pFiOriginal.Extension;
+            bool tryMoveOriginalBack = false, success = false;
+            try
+            {
+                pFiOriginal.MoveTo(tempfile);
+                tryMoveOriginalBack = true;
+                pFiNew.MoveTo(targetFullName);
+                success = true;
+            } 
+            catch (Exception)
+            {
+                success = false;
+            }
+
+            if (!success && tryMoveOriginalBack)
+            {
+                pFiOriginal.MoveTo(targetFullName); //failed to move the new file over, leave the original
+            }
+
+            try
+            {
+                File.Delete(tempfile);
+            }
+            catch(Exception){}
+
+            return success;
         }
 
 
